12446032d7fb68d3245c2f1b2ae09b3114533318
Marcosmv124 <105815107+Marcosmv124@users.noreply.github.com>
2022-05-27 11:52:47 -0700

    Add files via upload

---
+++ excepciones - cSharp/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace excepciones___cSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Martinez Valdez Marcos 21210398
+                try 
+                { 
+                Console.Write("Ingrese un numero: "); 
+                int Num1 = Int32.Parse(Console.ReadLine());
+
+                Console.Write("ingrese el siguiente Numero: ");
+                int Num2 = Int32.Parse(Console.ReadLine());
+
+                Console.WriteLine(Num1 / Num2);
+                Console.ReadLine();
+                }
+                catch(DivideByZeroException e)
+                {
+                    Console.WriteLine(e.Message); 
+                }
+                catch(FormatException e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+                Console.Write("Para salir del programa presione enter: "); 
+                Console.ReadKey(); 
+        }
+    }
+}
